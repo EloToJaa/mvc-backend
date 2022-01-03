7fec8807499440533725ab2301555afd74f83024
EloToJa <lukaszbudziak122@gmail.com>
2022-01-03 08:33:01 +0100

    remove db class

--- dev-wai/src/business.php
+++ dev-wai/src/business.php
@@ -3,25 +3,18 @@
 
 use MongoDB\BSON\ObjectID;
 
-class Database {
-    public $db;
+function init_db() {
+    $mongo = new MongoDB\Client(
+        "mongodb://localhost:27017/wai", [
+            'username' => 'wai_web',
+            'password' => 'w@i_w3b',
+        ]
+    );
+    
+    $db = $mongo->wai;
 
-    function __construct() {
-        $this->init_db();
-    }
-
-    function init_db() {
-        $mongo = new MongoDB\Client(
-            "mongodb://localhost:27017/wai", [
-                'username' => 'wai_web',
-                'password' => 'w@i_w3b',
-            ]
-        );
-        
-        $this->db = $mongo->wai;
-    }
+    return $db;
 }
-
 
 
 /*
